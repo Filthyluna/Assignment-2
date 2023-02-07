6a5baec6922379281ea040ad80e7b268beff4d7b
unknown <melanievasquez@nypl.org>
2023-02-07 12:22:10 -0500

    Finished map function

--- javascript-methods.js
+++ javascript-methods.js
@@ -11,22 +11,78 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
   // Place your code here.
+  let new_arr = [];
+  for (let i = 0; i < this.length; i++){
+    new_arr.push(callbackFn(this[i])); //Push the value from modified array into the empty array
+  }
+  return new_arr
 };
+
+//Test
+const arr = [1, 2, 3, 4];
+const map1 = arr.myMap(x => x + 2); //created
+console.log(map1);
 
 // FILTER //
 Array.prototype.myFilter = function(callbackFn) {
   // Place your code here.
+  let new_arr = []
+  for (let i = 0; i < this.length; i++){
+    if (callbackFn(this[i])) { //If it is true that this i value is fron cbfn...
+      new_arr.push(this[i]) //Push the item into the new array
+    }
+    else {
+      continue
+    }
+  }
+  return new_arr
 };
+
+//Test 
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.myFilter(word => word.length === 5);
+console.log(result);
 
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
   // Place your code here.
+  let bool = false
+  for (let i = 0; i < this.length; i++){
+    if (callbackFn(this[i])) {
+      bool = true
+    }
+    else {
+      continue
+    }
+  }
+  return bool;
 };
+
+//Test 
+const array = [1, 2, 3, 4, 5];
+// Checks whether an element is even
+const even = (element) => element % 2 === 0;
+console.log(array.mySome(even));
+// Checks if odd
+const odd = (element) => element % 2 === 1;
+console.log(array.mySome(odd));
 
 // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
   // Place your code here.
+  let bool = true
+  for (let i = 0; i < this.length; i++){
+    if (!callbackFn(this[i])){
+      bool = false
+    }
+  }
+  return bool;
 };
+
+// Test
+const isBelowThreshold = (currentValue) => currentValue < 40;
+const array1 = [1, 30, 39, 29, 10, 13];
+console.log(array1.myEvery(isBelowThreshold));
 
 // REDUCE //
 Array.prototype.myReduce = function(callbackFn) {
